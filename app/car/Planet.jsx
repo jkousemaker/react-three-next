b4c97b520b62e584523d398ea0e278c26182bd5c
jkousemaker <70757540+jkousemaker@users.noreply.github.com>
2024-05-01 20:05:21 +0200

    .

--- app/car/Planet.jsx
+++ app/car/Planet.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import { useFrame } from '@react-three/fiber'
-import { useTexture, Detailed } from '@react-three/drei'
+import { useTexture } from '@react-three/drei'
 import { editable as e } from '@theatre/r3f'
 
 export function Planet({ props }) {
@@ -19,11 +19,9 @@ function Model({ props }) {
     '/earth/normal.png',
   ])
   return (
-    <Detailed distances={[0, 10, 20]} {...props}>
-      <e.mesh theatreKey='Earth' position={[0, 0, 0]} scale={0.01} {...props}>
-        <sphereGeometry args={[200, 200, 200]} />
-        <meshStandardMaterial map={colorMap} normalMap={normalMap} occlusionMap={displacementMap} />
-      </e.mesh>
-    </Detailed>
+    <e.mesh theatreKey='Earth' position={[0, 0, 0]} scale={0.01} {...props}>
+      <sphereGeometry args={[200, 200, 200]} />
+      <meshStandardMaterial map={colorMap} normalMap={normalMap} occlusionMap={displacementMap} />
+    </e.mesh>
   )
 }
